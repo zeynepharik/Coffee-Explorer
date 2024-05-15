26aea472a12ffe62fac702f3833af904bdad7f87
zeynepharik <zeynepharik@gmail.com>
2024-05-15 18:39:19 +0300

    Kaydedildi

---
+++ 01-hello-world.js
@@ -0,0 +1,21 @@
+
+Boolean("11")
+
+console.log(
+    "price:"
+    typeof(price)
+
+)
+console.log(
+    "stringPrice:"
+    typeof(stringPrice)
+
+)
+
+console.log(
+    "hasPassword:"
+    typeof(hasPassword)
+    
+
+
+)
